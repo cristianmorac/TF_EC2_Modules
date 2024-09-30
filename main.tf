afe5d3d8f15c3eae9de9d0706068eb1df4189cc9
Cristian Mora <cristianfmorac@gmail.com>
2024-09-30 07:04:45 -0500

    install nginx in EC2

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ provider "aws" {
 resource "aws_instance" "nginx-server" {
   ami = "ami-0e86e20dae9224db8"
   instance_type = "t3.micro"
+  # ejecutar script en instancia
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt install -y nginx
+              sudo systemctl enable nginx
+              sudo systemctl start nginx
+              EOF
 }
 
 
