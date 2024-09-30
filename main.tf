d77af5f0b7b407987bb0bc5b9f118dffcb80f6ec
Cristian Mora <cristianfmorac@gmail.com>
2024-09-30 08:55:17 -0500

    create outputs

--- main.tf
+++ main.tf
@@ -75,5 +75,15 @@ resource "aws_security_group" "nginx-server-sg" {
 	}
 }
 
+output "server_public_ip" {
+	description = "IP pública instancia EC2"
+	value = aws_instance.nginx-server.public_ip
+}
+
+output "server_public_dns" {
+	description = "DNS público instancia EC2"
+	value = aws_instance.nginx-server.public_dns
+}
+
 
 
